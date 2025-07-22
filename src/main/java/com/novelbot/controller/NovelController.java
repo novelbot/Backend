bd931e35eb4550c1383b62ad8652a4644f2b54d4
jihoo <wlgn0432@gmail.com>
2025-07-23 00:35:19 +0900

    controller 빠트림!

--- src/main/java/com/novelbot/controller/NovelController.java
+++ src/main/java/com/novelbot/controller/NovelController.java
@@ -1,4 +1,26 @@
 package com.novelbot.controller;
 
+import com.novelbot.service.settingNovel.NovelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
 public class NovelController {
+    @Autowired
+    private NovelService novelService;
+
+    @PostMapping("/upload/novels")
+    public String uploadNovels(@RequestParam("file") MultipartFile file) {
+        try {
+            novelService.importFile(file);
+            return "Novels imported successfully!";
+        } catch (IOException e) {
+            return "Error importing novels: " + e.getMessage();
+        }
+    }
 }
