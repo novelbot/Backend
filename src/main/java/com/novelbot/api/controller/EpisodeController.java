d538cfbfc693eb92ed10b255d51f19d5b7fc9caf
jihoo <wlgn0432@gmail.com>
2025-07-23 23:36:08 +0900

    NOVELS DB 초기화

--- src/main/java/com/novelbot/api/controller/EpisodeController.java
+++ src/main/java/com/novelbot/api/controller/EpisodeController.java
@@ -2,7 +2,11 @@ package com.novelbot.api.controller;
 
 import com.novelbot.api.domain.Episode;
 import com.novelbot.api.service.settingEpisode.EpisodeService;
+import com.novelbot.api.utility.ImportEpisode.EpisodeImport;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,16 +18,21 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Component
+@EnableScheduling
 @RestController
 @RequestMapping("/api")
 public class EpisodeController {
     @Autowired
     private EpisodeService episodeService;
 
-    @GetMapping("/novels/{novel_Id}/episodes")
-    public List<Episode> getEpisodesByNovelId(@PathVariable Long novel_Id) throws IOException {
-        return episodeService.findEpisodesByNovelId(novel_Id);
-    }
+    @Autowired
+    private EpisodeImport episodeimport;
+
+//    @GetMapping("/novels/{novel_Id}/episodes")
+//    public List<Episode> getEpisodesByNovelId(@PathVariable Long novel_Id) throws IOException {
+//        return episodeService.findEpisodesByNovelId(novel_Id);
+//    }
 
     // @PostMapping("/upload/episodes")
     // public String uploadEpisodes(@RequestParam("file") MultipartFile file) {
@@ -33,4 +42,5 @@ public class EpisodeController {
     //         throw new RuntimeException(e);
     //     }
     // }
+
 }
