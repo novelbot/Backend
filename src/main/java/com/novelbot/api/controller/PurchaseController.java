74f11b035f841b3298415696ad09c7c112ed645b
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2025-07-24 13:24:38 +0900

    Feat: Controller 수정 중

---
+++ src/main/java/com/novelbot/api/controller/PurchaseController.java
@@ -0,0 +1,5 @@
+package com.novelbot.api.controller;
+
+public class PurchaseController {
+    
+}
---
+++ src/main/java/com/novelbot/api/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.novelbot.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+public class UserController {
+
+    // @Autowired
+    // private UserService userService;
+
+    // @PostMapping("/users"){
+    //     return userService
+    // }
+}
