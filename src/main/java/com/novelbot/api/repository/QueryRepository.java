06daaa3db54d91ea118026fb0c6a2340888a313e
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2025-07-31 15:28:43 +0900

    [Fix] Chatroom NoargsConstructor 추가, Queries import 수정

--- src/main/java/com/novelbot/api/repository/QueryRepository.java
+++ src/main/java/com/novelbot/api/repository/QueryRepository.java
@@ -1,7 +1,8 @@
 package com.novelbot.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface QueryRepository extends JpaRepository<Query, Integer> {
+import com.novelbot.api.domain.Queries;
+
+public interface QueryRepository extends JpaRepository<Queries, Integer> {
 }
