24e9a6fa41a2830d2e5e5ed6926b0d298b4e67b0
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2025-08-11 18:49:13 +0900

    [Fix] gradlew build 시 chat add

--- src/main/java/com/novelbot/api/repository/ReadingProgressRepository.java
+++ src/main/java/com/novelbot/api/repository/ReadingProgressRepository.java
@@ -4,5 +4,5 @@ import com.novelbot.api.domain.UserReadingProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReadingProgressRepository extends JpaRepository<UserReadingProgress, Long> {
-    // 필요한 쿼리 메소드 추가 (예: findByUserAndEpisode)
+    
 }
