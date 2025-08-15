3e0688157c438c52c63a91aa61d11cac09849427
jjjung0921 <earthenergy-1128@naver.com>
2025-08-15 18:17:36 +0900

    [Fix] CORS 에러 수정

--- src/main/java/com/novelbot/api/config/SecurityConfig.java
+++ src/main/java/com/novelbot/api/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configure(http))
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(formLogin -> formLogin.disable())
