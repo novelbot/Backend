f8efe81e48fb8384399d6462e0e9b4d5c50f061b
jjjung0921 <earthenergy-1128@naver.com>
2025-08-20 12:43:14 +0900

    [Feat] 토큰 재발급 로직 추가"

--- src/main/java/com/novelbot/api/dto/API/QueryAsk.java
+++ src/main/java/com/novelbot/api/dto/API/QueryAsk.java
@@ -9,8 +9,8 @@ public class QueryAsk {
     @JsonProperty("message")
     private String queryContent; //사용자가 질문한 내용
     
-    @JsonProperty("conversation_id")
-    private String beforeQuery; //해당 채팅방의 가장 최신 Query의 field 값(domain/Queries 참조, isUsingContext가 true일 경우에만 포함)
+    // @JsonProperty("conversation_id")
+    // private String beforeQuery; //해당 채팅방의 가장 최신 Query의 field 값(domain/Queries 참조, isUsingContext가 true일 경우에만 포함)
     
     @JsonProperty("episode_ids")
     private Integer[] isBoughtEpisodes; //사용자가 해당 소설에서 구매한 episode들의 id 모음
